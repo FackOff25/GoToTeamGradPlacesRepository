ee4bd2ae78611c52d2868aaa40fbadd982e7bbf5
FackOff <kozvladimir@bk.ru>
2023-10-14 15:28:31 +0300

    Echo starting

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,1 +1,25 @@
 package app
+
+import (
+	"log"
+
+	"github.com/labstack/echo/v4"
+)
+
+func Run() {
+
+	e := echo.New()
+
+	serverAddress := "0.0.0.0:8000"
+
+	if err := e.Start(serverAddress); err != nil {
+		log.Fatalf("error while starting server", err)
+	}
+}
+
+func configureServer(e *echo.Echo) error {
+
+	//e.GET("/api/v1/session/create", placesController.GetPlaces)
+
+	return nil
+}
